169a56fb73123c5027bc27fd4e4c3fbf0743835e
LMC <101943576+april808@users.noreply.github.com>
2023-04-09 03:42:43 +0800

    增加border-radius

--- demo/border/dist/script.js
+++ demo/border/dist/script.js
@@ -1,53 +1,107 @@
-var alldata= {
-shorthand: "border",
-property:[
-  {name: "border-width",type:"width",initial_value:"medium",
-      values:["medium","thin","thick","length"]},
-  {name: "border-color",type:"color",initial_value: "currentcolor",
-      values:["currentcolor"]},
-  {name: "border-style",type:"style",initial_value: "none",
-      values:["none","solid","dashed","dotted","double","groove","ridge","inset","outset"]}
-],
-notes: "邊框線"
+const alldata = {
+  shorthand: "border",
+  property: [
+    {
+      name: "border-width",
+      type: "width",
+      initial_value: "medium",
+      values: ["medium", "thin", "thick", "length"]
+    },
+    {
+      name: "border-style",
+      type: "style",
+      initial_value: "none",
+      values: [
+        "none",
+        "solid",
+        "dashed",
+        "dotted",
+        "double",
+        "groove",
+        "ridge",
+        "inset",
+        "outset"
+      ]
+    },
+    {
+      name: "border-color",
+      type: "color",
+      initial_value: "currentcolor",
+      values: ["currentcolor"]
+    },
+    {
+      name: "border-radius",
+      type: "width",
+      initial_value: "length",
+      values: [
+        "length"
+      ]
+    }
+  ],
+  notes: ["邊框線", "可以分別指定top,right,bottom,left"]
 };
 
 const app = Vue.createApp({
- // el: "#app",
-  data () {
+  // el: "#app",
+  data() {
     return {
       status: "",
       editing: false,
       showcode: true,
       cssSD: alldata,
-      defaultColor: 'currentcolor',
-      defaultwidth: 'length-defaultwidth'
-    }
+      defaultColor: "currentcolor"
+    };
   },
   methods: {
-    statusNow: function(notify){
+    checkshort: function () {
+      let check = this.cssSD.shorthand;
+      let name = this.cssSD.property[0].name;
+
+      if (check != "") {
+        return check;
+      } else {
+        return name;
+      }
+    },
+    statusNow: function (notify) {
       this.status = notify;
     },
-    valename: function(){
-      
-      var cssnames = []
-        var obj = this.cssSD.property
-        for(var i=0;i<obj.length;i++){
-          cssnames[i] = obj[i].name
-        }
-        return cssnames
+    propertyname: function () {
+      let cssnames = [];
+      let obj = this.cssSD.property;
+      for (let i = 0; i < obj.length; i++) {
+        cssnames[i] = obj[i].name;
+      }
+      return cssnames;
+    },
+    tocheckinput: function (val,attr) {
 
-    },
-    tostring: function(val){
-      return val.replace(/'"'/g,'')
-    },
-    width: function (val){
-      var wnumber = this.cssSD.property[0].values[3]
-      var valspace = this.cssSD.property[0].initial_value
+      let length = attr.length-1
+      let nowindex = attr.indexOf(val)
       
-      if( wnumber != val && wnumber != '' ){
-        return valspace = wnumber
-      }else{
-        return valspace
+      if (val=='length' || nowindex == length  ) { return true }
+      // else if (nowindex == length) { return true }
+      else { return false }
+      
+    },
+    tocheckvalue: function (item) {
+      let obj = this.cssSD.property;
+      
+      /* 確認屬性名稱的指定 */
+      let nowindex = 0
+      for (let i = 0; i < obj.length; i++) { 
+        if(  item == obj[i].name  ){
+          nowindex = i
+        }
+      }/* for結束 確認nowindex */
+      
+      /* 屬性預設值 和可改動的內容 */
+      let valspace = obj[nowindex].initial_value;
+
+      if ( valspace != "") {
+        return valspace;
+      } else {
+        return ""
       }
     },
     // updateColorValue(event) {
@@ -55,20 +109,43 @@ const app = Vue.createApp({
     // }
   },
   computed: {
-    getStyle(){
-      return {
-        "border-width": this.width('length'),
-        "border-color": this.defaultColor,
-        "border-style": this.cssSD.property[2].initial_value,
-      }
+    gettitle() {
+      return this.checkshort();
     },
-    getshort(){
-      var name = this.cssSD.shorthand
-      return  name +': '+ this.width('length') + ' ' + this.defaultColor + ' ' +  this.cssSD.property[2].initial_value +";"
+    getStyle() {
+      let nowCSS = this.propertyname()
+      
+      let attr = []
+      for(let item of nowCSS ){
+        
+        attr[nowCSS.indexOf(item)] = item + ": " + this.tocheckvalue(item)
+      }
+      
+      let obj = attr.join(';\n')
+      return obj
+    },
+    getshort() {
+      let attr = []
+      let name = this.cssSD.shorthand;
+//       let nowCSS = this.propertyname()
+      
+//       for(let item of nowCSS ){
+//         attr[nowCSS.indexOf(item)] = " " + this.tocheckvalue(item)
+//       }
+//       return ( name + ": " + attr.join('\n')
+      return(
+        name +
+        ": " + this.tocheckvalue("border-width") +
+        " " +
+        this.tocheckvalue("border-style") +
+        " " +
+        this.tocheckvalue("border-color") +
+        ";\n"+
+        'border-radius: '+this.tocheckvalue("border-radius")+";\n"
+      );
     }
   }
-})
-app.mount('#app');
-
+});
+app.mount("#app");
 
 // ------------------------
